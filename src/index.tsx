ee135b1aa979ac0ebb754b36b936cd5abb0b8742
Kohlenbaron28 <110342046+Kohlenbaron28@users.noreply.github.com>
2023-05-07 17:03:28 +0300

    styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import useMockAdapter from "./api/useMockAdapter";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
